f208cf9ad379a75355cf12a310df71d29f2486ef
Diego Cabanillas <dg.cabanillas@uni.pe>
2021-05-28 09:09:23 -0500

    Adding home temporal buttons

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import MainLayout from 'components/layout/MainLayout';
+import React, { useContext } from 'react';
+import MainLayout   from 'components/layout/MainLayout';
+import CLink        from 'components/customized/CLink';
+import RouterContext from 'src/context/router/router.context';
 
 const Home = () => {
+    const { gotoLogin, gotoRegister, gotoRecover } = useContext(RouterContext)
+
     return (
-        <div>
-            
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <CLink  
+                text='INGRESAR'
+                onClick={gotoLogin}
+                style={{ fontSize: 20 }}
+            />
+            <CLink  
+                text='REGISTRAR'
+                onClick={gotoRegister}
+                style={{ fontSize: 20 }}
+            />
+            <CLink  
+                text='RECUPERAR CUENTA'
+                onClick={gotoRecover}
+                style={{ fontSize: 20 }}
+            />
         </div>
     )
 }
